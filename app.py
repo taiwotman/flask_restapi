b895e93c8b0817d17505fe22f2a718ab071870fa
Taiwo O. Adetiloye <taiwotman@users.noreply.github.com>
2020-02-25 13:25:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ from apis import blueprint as api
 if __name__ == '__main__':
     app = Flask(__name__)
     app.register_blueprint(api)
-    app.run(debug=True)  # http://127.0.0.1:5000/api/1/
+    #...
+    # app.register_blueprint(api2)
+    #app.register_blueprint(apiX)
+    app.run(debug=True)  # http://127.0.0.1:5000/api/v1/
 
