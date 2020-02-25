8c69e534f6c0b085071a555c63ed82bce8511a97
Taiwo O. Adetiloye <taiwotman@users.noreply.github.com>
2020-02-25 12:48:44 -0600

    Add files via upload

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,11 +2,10 @@ from flask import Blueprint
 from flask_restplus import Api
 from .namespace1 import api as ns1
 # from .namespace2 import api as ns2
-# # ...
+# ...
 # from .namespaceX import api as nsX
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
-# api_v1 = Api(blueprint)
 
-api = Api(blueprint, title='My Title',version='1.0', description='A description')  # All API metadatas
+api = Api(blueprint, title='TODO API',version='1.0', description='A description')  # All API metadatas
 
 api.add_namespace(ns1)
