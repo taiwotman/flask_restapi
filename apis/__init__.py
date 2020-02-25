51a6cf41213df667a411d4f0d91afabd86274d51
Taiwo O. Adetiloye <taiwotman@users.noreply.github.com>
2020-02-25 13:23:13 -0600

    Update __init__.py

--- apis/__init__.py
+++ apis/__init__.py
@@ -6,6 +6,7 @@ from .namespace1 import api as ns1
 # from .namespaceX import api as nsX
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
-api = Api(blueprint, title='TODO API',version='1.0', description='A description')  # All API metadatas
+api = Api(blueprint, title='TODO API',version='1.0',
+    description='Based on the Flask-RESTPlus, an extension for Flask that adds support for quickly building REST APIs while encouraging best practices with minimal setup.')  # All API metadatas
 
 api.add_namespace(ns1)
